8c690ed54393aa1fe04d1baf2d0468999e2dfb74
Md. Zahid-Hossain Shahin <126140511+24shahin@users.noreply.github.com>
2024-03-19 16:37:14 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Images from "./Images";
 import profile from "../assets/profile.png";
+import { FaHome } from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
 
 function Navbar() {
   return (
     <div className="navbox">
       <Images src={profile} style={"userImg"} />
+      <FaHome className="icon"/>
     </div>
   );
 }
