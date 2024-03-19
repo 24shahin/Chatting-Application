9029767ceb8145f22343ed31480e5c5c32a79161
Md. Zahid-Hossain Shahin <126140511+24shahin@users.noreply.github.com>
2024-03-19 11:32:41 +0600

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Registration from "./Pages/Registration";
 import { ToastContainer } from "react-toastify";
 import ForgotPassword from "./Pages/ForgotPassword";
+import Massage from "./Pages/Massage";
+import Setting from "./Pages/Setting";
+import Pages from "./Pages/Pages";
 
 function App() {
   const router = createBrowserRouter(
@@ -17,7 +19,10 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/" element={<Registration />}></Route>
         <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
-        <Route path="/pages" element={<Home />}></Route>
+        <Route path="/pages" element={<Pages />}>
+          <Route path="massage" element={<Massage />}></Route>
+          <Route path="setting" element={<Setting />}></Route>
+        </Route>
       </>
     )
   );
